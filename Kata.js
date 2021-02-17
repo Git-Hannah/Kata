fdf36cfad327ee1ca7b4a1fc5d99959a2b566a28
Hannah <hannahsenft@gmx.net>
2021-02-17 11:02:46 +0100

    Who is the killer

--- Kata.js
+++ Kata.js
@@ -208,3 +208,23 @@ function spoonerize(words) {
 spoonerize("nit picking")
 //"pit nicking"
 
+
+//Who is the killer?
+//Some people have been killed!
+//You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+
+function killer(suspectInfo, dead) {
+  for(let suspect in suspectInfo ){
+    if(includesDead(suspectInfo[suspect],dead))//object[key] object.key
+    return suspect
+  }
+}
+function includesDead(arr1,arr2){
+  let out=true
+    arr2.forEach(el=>{if(!arr1.includes(el)) out=false})
+    return out
+} 
+
+killer({'James': ['Jacob', 'Bill', 'Lucas'], 'Johnny': ['David', 'Kyle', 'Lucas'], 'Peter': ['Lucy', 'Kyle']}, ['Lucas', 'Bill'])
+//'James'
+
